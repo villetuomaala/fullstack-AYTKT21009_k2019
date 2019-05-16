8082fe3ad9b9dbe87089d309311b44e48f8143f5
Ville Tuomaala <ville.tuomaala@almamedia.fi>
2019-05-17 00:20:48 +0300

    Add unicafe

---
+++ osa1/unicafe/src/index.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = (props) => {
+    return (
+        <button onClick={props.handleClick}>
+            {props.value}
+        </button>
+    )
+}
+
+const Header = (props) => {
+    return (
+        <h1>{props.value}</h1>
+    )
+}
+
+const Statistics = (props)  => {
+    return (
+        <div>
+            <span>{props.text}</span> <span>{props.value}</span>
+        </div>
+    )
+}
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const setAnswerValue = (valueType) => {
+    switch(valueType) {
+        case 'good':
+            setGood(good+1)
+            break;
+        case 'neutral':
+            setNeutral(neutral+1)
+            break;
+        case 'bad':
+            setBad(bad+1)
+            break;
+        default:
+            throw "Not supported"
+      }
+  }
+
+  return (
+    <div>
+      <Header value="anna palautetta"/>
+      <Button handleClick={() => (setAnswerValue('good'))} value="hyvä"/>
+      <Button handleClick={() => (setAnswerValue('neutral'))} value="neutraali"/>
+      <Button handleClick={() => (setAnswerValue('bad'))} value="huono"/>
+
+      <Header value="statistiikka"/>
+      { good+neutral+bad !== 0 ?
+        <div>
+            <Statistics text="hyvä" value={good}/>
+            <Statistics text="neutraali" value={neutral}/>
+            <Statistics text="huono" value={bad}/>
+            <Statistics text="yhteensä" value={good-bad}/>
+            <Statistics text="keskiarvo" value={(good-bad)/(good+neutral+bad)}/>
+            <Statistics text="positiivisia" value={(good/(good+neutral+bad)*100).toString()+'%'}/>
+        </div>
+        :
+        <div>Ei yhtään palautetta annettu</div>
+      }
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
