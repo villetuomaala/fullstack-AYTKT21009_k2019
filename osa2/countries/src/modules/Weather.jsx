23f263c6229c30023b1cba6ac55dcc025b99ae5e
Ville Tuomaala <ville.tuomaala@almamedia.fi>
2020-01-23 08:48:11 +0200

    tidy

--- osa2/countries/src/modules/Weather.jsx
+++ osa2/countries/src/modules/Weather.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 
 const Weather = ({ data }) => {
-    if (data.length === 1 ) {
-      return (
-        <div>
-          <h3>Weather in {data[0].location.name}</h3>
-          <div>temperature {data[0].current.temperature} &deg;C</div>
-          <div>{data[0].current.weather_icons.map(i => <img src={i} width="5%" alt={data[0].current.weather_descriptions.join(", ")}/> )}</div>
-          <div>{data[0].current.weather_descriptions.join(", ")}</div>
-        </div>
-      )
-    } else return ''
+  if (data.length === 1 ) {
+    const weatherData = data[0]
+
+    return (
+      <div>
+        <h3>Weather in {weatherData.location.name}</h3>
+        <div>temperature {weatherData.current.temperature} &deg;C</div>
+        <div>{weatherData.current.weather_icons.map(i => <img src={i} width="5%" alt={weatherData.current.weather_descriptions.join(", ")}/> )}</div>
+        <div>{weatherData.current.weather_descriptions.join(", ")}</div>
+      </div>
+    )
+  } else return ''
 }
 
 export default Weather
